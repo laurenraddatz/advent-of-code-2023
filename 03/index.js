7c3f9d7d1aad39b9efed45602d6281a68274d067
Lauren Raddatz <lraddatz@squareup.com>
2023-12-03 23:59:13 -0500

    day three part two

--- 03/index.js
+++ 03/index.js
@@ -2,9 +2,9 @@ import { readFileSync } from 'fs'
 
 const input = readFileSync('03/input.txt').toString().split('\n')
 
-let symbols = {}
-let numbers = []
 const p1 = (schematic) => {
+  let symbols = {}
+  let numbers = []
   schematic.map((line, i) => {
     let number = {
       digits: [],
@@ -52,3 +52,61 @@ const p1 = (schematic) => {
 }
 
 console.log('part one:', p1(input)) // 553079
+
+const p2 = (schematic) => {
+  let symbols = {}
+  let numbers = []
+  schematic.map((line, i) => {
+    let number = {
+      digits: [],
+      row: i,
+      col: []
+    }
+    for (let j = 0; j < line.length; j++) {
+      const char = line[j]
+      if (char >= '0' && char <= '9') {
+        number.digits.push(char)
+        number.col.push(j)
+      } else {
+        if (number.digits.length > 0) {
+          numbers.push(number)
+          number = {
+            digits: [],
+            row: i,
+            col: []
+          }
+        }
+        if (char !== '.') {
+          symbols[`${i},${j}`] = {
+            symbol: char,
+            row: []
+          }
+        }
+      }
+    }
+    if (number.digits.length > 0) {
+      numbers.push(number)
+    }
+  })
+
+  numbers.map((n) => {
+    for (let j of n.col) {
+      for (let rd of [-1, 0, 1]) {
+        for (let cd of [-1, 0, 1]) {
+          const s = symbols[`${n.row + rd},${j + cd}`]
+          if (s && s.symbol === '*') {
+            s.row.push(Number(n.digits.join('')))
+            return
+          }
+        }
+      }
+    }
+  })
+
+  return Object.values(symbols)
+    .filter((s) => s.row.length === 2)
+    .map((s) => s.row[0] * s.row[1])
+    .reduce((a, b) => a + b, 0)
+}
+
+console.log('part two:', p2(input)) // 84363105
